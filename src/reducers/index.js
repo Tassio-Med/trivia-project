399833639029fa6be442350f8682603a9958f941
Alexandre Sullivan Teixeira <ast.sullivan@gmail.com>
2022-04-05 19:19:14 -0300

    Main group 19 requisito 02 Feito requisição Api Trivia para a chave token Implementamos button Play reducer play

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
+import user from './user';
+import player from './player';
+import token from './token';
 
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({
+  user,
+  player,
+  token,
+});
 
 export default rootReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -5,4 +5,8 @@ import rootReducer from '../reducers/index';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+if (window.Cypress) {
+  window.store = store;
+}
+
 export default store;
