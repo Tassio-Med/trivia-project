455ff7322fd18c3696e24f2bb964cd4b775d90dd
Rafael Godoy <rafa.qfg@gmail.com>
2022-04-08 15:54:18 -0300

    main-group-19-requisito-12

---
+++ src/pages/FeedBack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class FeedBack extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default FeedBack;
